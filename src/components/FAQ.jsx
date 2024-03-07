87482a0c63a7ec14c32e6b729993afbcd3021295
Izam Ghali <izamghali18@gmail.com>
2024-03-07 13:31:08 +0700

    STYLE: added text-slate-50 on connect btn on FAQ & svg fill

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -105,8 +105,8 @@ export default function FAQ({ videoIndex }) {
                         </svg>
                         Projects
                     </button>
-                    <button  className="faq-btn btn btn-neutral group">Connect
-                        <svg className="bi bi-arrow-up-right w-4 group-hover:translate-x-1 group-hover:-translate-y-1 duration-200" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <button  className="faq-btn btn btn-neutral text-slate-50 group">Connect
+                        <svg className="w-4 group-hover:translate-x-1 group-hover:-translate-y-1 duration-200 fill-slate-50" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
                         </svg>
                     </button>
