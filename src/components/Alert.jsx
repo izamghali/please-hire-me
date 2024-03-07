2029be12359767de75b8e9b58d5319703014b0cb
Izam Ghali <izamghali18@gmail.com>
2024-03-07 12:44:40 +0700

    STYLE: adjusted deny btn bg color on Alert component based on ocean video

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -20,19 +20,19 @@ export default function Alert({ videoIndex }) {
     return (
         <div className="relative flex justify-center ">
             <div role="alert" className={`
+                ${ videoIndex === 5 || videoIndex === 0 ? 'border-slate-50 bg-transparent text-slate-50' : 'text-slate-800 bg-[rgba(255,255,255,0.4)]' } 
                 alert z-10 absolute
                 bg-transparent backdrop-blur-[0.5px] 
                 h-fit w-fit border-[1px] border-black scale-90 xl:scale-100 
                 p-3 xl:p-4 duration-200 
                 
-                ${ videoIndex === 5 || videoIndex === 0 ? 'border-slate-50 bg-transparent text-slate-50' : 'text-slate-800 bg-[rgba(255,255,255,0.4)]' } 
                 
                 
             `}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 <span className="">Play background music?</span>
                 <div className="flex gap-2">
-                    <button onClick={popOut} className={`btn btn-sm ${ videoIndex === 5 || videoIndex === 0 ? 'border-slate-50 bg-transparent text-slate-50 hover:bg-slate-50 hover:text-slate-800' : '' } cursor-pointer bg-transparent border-[1px] border-slate-800 text-slate-800 hover:text-white hover:bg-slate-800`}>Deny</button>
+                    <button onClick={popOut} className={`btn btn-sm ${ videoIndex === 5 || videoIndex === 0 ? 'border-slate-50 bg-transparent text-slate-50 hover:bg-slate-50 hover:text-slate-800' : 'bg-transparent border-slate-800 text-slate-800 hover:text-slate-50 hover:bg-slate-800' } cursor-pointer border-[1px]`}>Deny</button>
                     <button onClick={autoPlay} className="btn btn-sm btn-primary cursor-pointer">Accept</button>
                 </div>
             </div>
